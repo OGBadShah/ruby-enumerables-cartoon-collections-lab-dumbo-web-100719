aae0f1622000cc2c41bd780c69c4f52b448ac670
iamnabeel2 <iamnabeel2@gmail.com>
2019-10-03 04:25:05 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,7 +11,7 @@ def summon_captain_planet(planeteer_calls)# code an argument here
 end
 
 def long_planeteer_calls(array)# code an argument here
-  if array.each {|x| x > 4 }
+  if array.bsearch {|x| x > 4 }
     return true
   else
     return false
