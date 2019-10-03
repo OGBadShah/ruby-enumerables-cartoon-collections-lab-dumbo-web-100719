7e5191981d615f966c243801c3eb646599dca916
iamnabeel2 <iamnabeel2@gmail.com>
2019-10-03 04:29:44 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,7 +11,7 @@ def summon_captain_planet(planeteer_calls)# code an argument here
 end
 
 def long_planeteer_calls(array)# code an argument here
-  array.any? { |word| word.length > 4  }
+  array.any? { |word| word.length > 4 }
   # Your code here
 end
 
